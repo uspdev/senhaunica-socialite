b4ac0cab3919e9ba6aa1002bb54eb14a7c0c78fa
Masaki Kawabata Neto <kawabata@usp.br>
2021-07-23 10:32:40 -0300

    reorganzando config

--- config/config.php
+++ config/config.php
@@ -4,13 +4,13 @@ return [
     // para rotas internas
     'routes' => true, // usa rotas e controller internos
     'prefix' => '', // coloque um prefixo em caso de colisão de rotas (login, callback, logout e users).
-    'userRoutes' => 'users', // define as rotas para o gerenciador de usuários interno
+    'userRoutes' => 'users', // define as rotas para o gerenciador de usuários interno, dentro de prefix
     'middleware' => ['web'], // you probably want to include 'web' here
     'session_key' => 'senhaunica-socialite', // chave da sessão. Troque em caso de colisão com outra variável de sessão.
     'template' => 'laravel-usp-theme::master', // template a ser estendido para as views internas
 
-    // usa as permissoes internas, padrão para v3.
-    // Se false não usará permission ao efetuar login
+    // usa as permissoes internas, padrão para v4.
+    // Se false, não usará permission ao efetuar login
     'permission' => true,
 
     // permite login somente de usuários já cadastrados na base local ou autorizados nos admins, gerentes ou users
@@ -19,8 +19,9 @@ return [
     // se true, habilita botão para remover usuário (destroy)
     'destroyUser' => false,
 
-    // se true, revoga as permissões do usuario se não estiver no env
-    'dropPermissions' => false,
+    // se true, revoga as permissões do usuario se não estiver no env.
+    // quer dizer que as permissões serão gerenciadas todas a partir do env da aplicação.
+    'dropPermissions' => env('SENHAUNICA_DROP_PERMISSIONS', false),
 
     // cadastre os admins separados por virgula
     'admins' => array_map('trim', explode(',', env('SENHAUNICA_ADMINS', ''))),
@@ -34,4 +35,6 @@ return [
     'dev' => env('SENHAUNICA_DEV', 'no'),
     'debug' => (bool) env('SENHAUNICA_DEBUG', false),
     'callback_id' => env('SENHAUNICA_CALLBACK_ID'),
+
+    // SENHAUNICA_KEY e SENHAUNICA_SECRET são carregados em services.php da biblioteca
 ];
