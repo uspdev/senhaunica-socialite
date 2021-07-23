df89cd41ba932e87a1cff2dbad53b3acc26977b3
Masaki Kawabata Neto <kawabata@usp.br>
2021-07-23 09:33:06 -0300

    arquivo desnecessário

--- resources/views/partials/unavailable.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="alert alert-danger mt-4">
-  <div class="alert-heading h4">
-    Serviço indisponível!
-  </div>
-  Inclua no seu arquivo App\Models\User o trait da senhaunica:
-  <div class="ml-3">use \Uspdev\SenhaunicaSocialite\Traits\HasSenhaunica;</div>
-</div>
