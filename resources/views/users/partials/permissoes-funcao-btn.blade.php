2e4f0af24ed6d5fde7757ba69f92a1f4ab72987e
Masaki K Neto <kawabata@usp.br>
2023-04-18 10:12:24 -0300

    corrigindo namespace

--- resources/views/users/partials/permissoes-funcao-btn.blade.php
+++ resources/views/users/partials/permissoes-funcao-btn.blade.php
@@ -1,4 +1,4 @@
-@foreach ($user->roles->where('guard_name', 'app')->all() as $r)
+@foreach ($user->roles->all() as $r)
 <button data-user-id="{{ $user->id }}" data-route="{{ route('senhaunica-users.show', $user->id) }}"
   class="btn btn-sm btn-secondary py-0 senhaunicaUserPermissionBtn" title="Funções">
   {{ $r->name }}
